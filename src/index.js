b4f5df5f51de7dd27edf9935c3af64c75110f349
retater56 <70779585+retater56@users.noreply.github.com>
2021-02-13 18:34:57 +0300

    feat: create a task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,58 @@
+ const a = {
+      1: 'one',
+      2: 'two',
+      3: 'three',
+      4: 'four',
+      5: 'five',
+      6: 'six',
+      7: 'seven',
+      8: 'eight',
+      9: 'nine',
+      10: 'ten',
+      11: 'eleven',
+      12: 'twelve',
+      13: 'thirteen',
+      14: 'fourteen',
+      15: 'fifteen',
+      16: 'sixteen',
+      17: 'seventeen',
+      18: 'eighteen',
+      19: 'nineteen',
+      20: 'twenty',
+      30: 'thirty',
+      40: 'forty',
+      50: 'fifty',
+      60: 'sixty',
+      70: 'seventy',
+      80: 'eighty',
+      90: 'ninety',    
+}
+
 module.exports = function toReadable (number) {
   
+    unit = number % 10;
+    ten = (number - unit) % 100;
+    ten20 = number % 100;
+    hundred = (number - unit - ten) / 100;
+
+    let result = "";
+
+    if (number === 0) {
+        result = "zero";
+    }
+    if(number >= 100) {
+        result = a[hundred] + " hundred";
+    }
+    if (ten < 20 && ten) {
+        result +=" " + a[ten20];
+    } else {
+        if (ten) { 
+            result += " " + a[ten];
+        }
+        if (unit) {
+            result += " " + a[unit];
+        }
+    }
+
+    return result.trim();
 }
